e582ae385554994bdc14a97a5fea5daae27423d0
Berkan Kabadayı <kabadayiberkan@gmail.com>
2026-04-09 18:18:52 +0300

    feat: initial setup and database configuration

---
+++ knexfile.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export default {
+  production: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+
+    migrations: {
+      directory: "./src/migrations",
+    },
+  },
+};
